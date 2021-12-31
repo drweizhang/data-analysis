34a6be1c104b4c58c719e59203bc68108b91a69b
Sixuan Liu <sixuan@giphy.com>
2021-12-31 11:32:14 -0500

    add common

---
+++ common/text_preprocessing.py
@@ -0,0 +1,14 @@
+from ntlk.corpus import stopwords
+from Collections import counter
+
+
+text = """The cat is in the box. The cat likes the box.                   The box is over the cat."""
+
+# Tokenize the text to words, and make them lower case. Store all the alphabetical tokens in var token. 
+tokens = [w for w in word_tokenize(text.lower()) if w.isalpha()] 
+
+# Remove all the stopwords from tokens 
+no_stops = [t for t in tokens if t notin stopwords.words('english')]
+
+# Count the most common used 2 tokens
+Counter(no_stops).most_common(2)
